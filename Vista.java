c6fe1b694ed37548cf76ad26072a85efe55ee081
Francisco Castillo <cas21562@uvg.edu.gt>
2021-09-03 15:26:23 -0600

    Created Classes
    Creation of Classes that we will need

---
+++ Vista.java
@@ -0,0 +1,40 @@
+/*
+
+    NOMBRE DE LA CLASE: Vista.java
+    PROGRAMADOR: FGrancisoc Castillo 21562
+    HISTORIAL DE MODIFICACIONES:
+        -Se incluyo un metodo de Saludo y despedida
+
+
+*/
+import java.util.Scanner;
+public class Vista {
+    private Scanner scan;
+    public Vista(){
+        scan = new Scanner(System.in);
+    }
+
+    public void saludar(){
+        System.out.println("Hola! Bienvenido al parqueo.");
+    }
+
+    public int mostrarMenu(){
+        try{
+            int opcion;
+            System.out.println("Que desea hacer ahora? (Ingrese el numero de opcion que desea)");
+            System.out.println("1. Parquear un carro");
+            System.out.println("2. Retirar un carro del parqueo");
+            System.out.println("3. Ampliar parqueo");
+            System.out.println("4. Mostrar horarios de mayor utilizacion");
+            System.out.println("5. Mostrar el tiempo promedio en parqueo");
+            System.out.println("6. Mostrar el parqueo mas utilizado");
+            System.out.println("7. Mostrar cantidad de vehiculos rechazados");
+            System.out.println("8. Mostrar la marca de carros mas utilizada");
+            System.out.println("9. Caracteristicas del parqueo mas utilizado");
+            System.out.println("10. Salir");
+        }catch(Exception e){
+
+            return -1;
+        }
+    }
+}
